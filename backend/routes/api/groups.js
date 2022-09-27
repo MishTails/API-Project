a977827476c29f4716d0f59a2d3f98592defb619
Jarrod Mishima <mishimajam@gmail.com>
2022-09-26 21:19:43 -0700

    get details of a Group from an Id

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,17 +1,19 @@
-const express = require('express')
+const express = require('express');
+const { where } = require('sequelize');
 const router = express.Router();
 
 const {Attendance, Event, EventImage, Group, GroupImage, Membership, User, Venue} = require("../../db/models")
 // get all groups
 router.get('/', async (req, res) => {
-  const group = await Group.findAll({
+  const groups = await Group.findAll({
     include: {
       model: GroupImage,
-      attributes: ['previewImage'],
-      where: GroupImage.groupId === Group.groupId
+      attributes: ['url'],
+      where: GroupImage.groupId === Group.groupId,
     }
   })
-  return res.json(group)
+
+  return res.json(groups)
 })
 // // get all groups by current user
 router.get('/current', async (req, res) => {
@@ -20,13 +22,37 @@ router.get('/current', async (req, res) => {
     const groups = await Group.findAll({
       include: {
         model: GroupImage,
-        attributes: ['previewImage'],
+        attributes: ['url'],
         where: GroupImage.groupId === Group.groupId
       },
       where: {
         organizerId: user.id
       }
     })
+
+    return res.json(groups)
   }
 })
 // get details of a Group from an Id
+router.get('/:groupId', async (req, res) => {
+  const groups = await Group.findByPk(req.params.groupId, {
+    include: [
+      {
+        model: GroupImage,
+        attributes: ['id', 'url', 'preview'],
+      },
+      {
+        model: User,
+        as: 'Organizer',
+        attributes: ['id', 'firstName', 'lastName'],
+      },
+      {
+        model: Venue,
+        attributes: ['id', 'groupId', 'address', 'city', 'state', 'lat', 'lng'],
+      }
+  ]
+  })
+
+  return res.json(groups)
+})
+module.exports = router;
