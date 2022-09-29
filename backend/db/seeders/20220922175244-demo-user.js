0a084449f6f6ee1527dfc297daa83a05ee2a83a1
Jarrod Mishima <mishimajam@gmail.com>
2022-09-29 13:23:07 -0700

    resetting database main

--- backend/db/seeders/20220922175244-demo-user.js
+++ backend/db/seeders/20220922175244-demo-user.js
@@ -9,26 +9,6 @@ module.exports = {
         firstName: 'Demo',
         lastName: 'Lition',
         username: 'Demo-lition',
-<<<<<<< HEAD
-        firstName: 'Demo',
-        lastName: 'Lition',
-        hashedPassword: bcrypt.hashSync('password')
-      },
-      {
-        email: 'user1@user.io',
-        username: 'FakeUser1',
-        firstName: 'Fake',
-        lastName: 'User',
-        hashedPassword: bcrypt.hashSync('password2')
-      },
-      {
-        email: 'user2@user.io',
-        username: 'FakeUser2',
-        firstName: 'One',
-        lastName: 'Two',
-        hashedPassword: bcrypt.hashSync('password3')
-      }
-=======
         email: 'demo@user.io',
         hashedPassword: bcrypt.hashSync('password')
       },
@@ -39,7 +19,6 @@ module.exports = {
         email: 'jkmish@gmail.com',
         hashedPassword: bcrypt.hashSync('password')
       },
->>>>>>> dev
     ], {});
   },
 
