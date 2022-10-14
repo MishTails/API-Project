c5530d28d3797259de1c92fdb6aa579d45ab53d9
Jarrod Mishima <mishimajam@gmail.com>
2022-10-14 13:59:15 -0700

    phase 3 done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -37,7 +37,30 @@ export const restoreUser = () => async dispatch => {
   dispatch(setUser(data.user));
   return response;
 };
+
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
 const initialState = { user: null };
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
 
 const sessionReducer = (state = initialState, action) => {
   let newState;
