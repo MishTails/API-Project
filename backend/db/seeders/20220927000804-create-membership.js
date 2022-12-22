8cbc63d4ffbd7791c0271bd0b92d68e179754602
Jarrod Mishima <mishimajam@gmail.com>
2022-12-22 14:28:20 -0800

    membership attendence seeder bolster

--- backend/db/seeders/20220927000804-create-membership.js
+++ backend/db/seeders/20220927000804-create-membership.js
@@ -17,7 +17,43 @@ module.exports = {
         userId: 2,
         groupId: 1,
         status: "co-host"
-      }
+      },
+      {
+        userId: 2,
+        groupId: 2,
+        status: "member"
+      },
+      {
+        userId: 3,
+        groupId: 3,
+        status: "member"
+      },
+      {
+        userId: 4,
+        groupId: 4,
+        status: "member"
+      },
+      {
+        userId: 5,
+        groupId: 5,
+        status: "member"
+      },
+      {
+        userId: 6,
+        groupId: 6,
+        status: "member"
+      },
+      {
+        userId: 7,
+        groupId: 7,
+        status: "member"
+      },
+      {
+        userId: 8,
+        groupId: 8,
+        status: "member"
+      },
+
     ], {});
   },
 
